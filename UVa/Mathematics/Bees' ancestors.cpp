802dd9802d0fea7ca880a6f1daba087631900e06
Unknown <santosdedeusm@gmail.com>
2018-02-13 22:39:05 -0200

    UVa - 12459

---
+++ UVa/Mathematics/Bees' ancestors.cpp
@@ -0,0 +1,27 @@
+/*UVa - 12459 - Bees' ancestors*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int vet[90];
+
+long long int fib(int n){
+	//~ if (n==1) return 1;
+	//~ else if(n==2) return 2;
+	if(vet[n]==-1)
+		vet[n] = fib(n-1)+fib(n-2);
+	return vet[n];
+}
+
+int main(){
+	int n;
+	memset(vet,-1,sizeof vet);
+	while(scanf("%d",&n),n){
+		vet[1] = 1;
+		vet[2] = 2;
+		if(vet[n]==-1) fib(n);
+		printf("%lld\n",vet[n]);
+		puts("");
+	}
+	return 0;
+}
